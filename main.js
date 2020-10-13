f94261a3b97a89d0bba786da73b0d1fd4d75eaca
Unknown <blossomperez18@gmail.com>
2020-10-13 08:39:37 -0500

    Add click event to checkbox

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ const toggle = document.querySelector('input[checkbox]');
 const annual = document.querySelector('annual-price');
 const monthly = document.querySelector('monthly-price');
 
+const monthlyAnnualPrices = () => {
+    if(toggle.checked) {
+        annual.style.display = "block";
+        monthly.style.display = "none";
+    }else {
+        annual.style.display = "none";
+        monthly.style.display = "block";
+    }
+}
+
+toggle.addEventListener('click', monthlyAnnualPrices)
